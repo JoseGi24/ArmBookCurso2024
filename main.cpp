ad01e4c5f8311f00f40f33f69d0ceada00f5816d
JoseGi24 <josegiachino@yahoo.com.ar>
2024-07-05 23:05:08 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include "mbed.h"
 
-// Ire modificando este archivo segun mi tp, solo modifique el led para pedir revision.
+// Ire modificando este archivo segun mi tp, solo modifique el led2 para pedir revision.
 
 int main()
 {
